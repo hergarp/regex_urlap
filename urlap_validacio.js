3b162480774b71f0aacd3d4de4b14bf1457738e8
hergarp <78268365+hergarp@users.noreply.github.com>
2021-04-23 23:35:46 +0200

    ellenorzesek_beepitve

--- urlap_validacio.js
+++ urlap_validacio.js
@@ -13,9 +13,14 @@ function validate() {
     var urlapAdatok = "";
     var nevInput = ID("nev").value;
     var emailInput = ID("email").value;
+    var email_ujraInput = ID("email_ujra").value;
+    var telefonInput = ID("telefon").value;
+    var webInput = ID("web").value;
     console.log(nevInput);
     console.log(emailInput);
-
+    console.log(email_ujraInput);
+    console.log(telefonInput);
+    
 
     if (nevInput.lenght < 3) {
         hiba += "A név legyen 3 karakternél hosszabb!";
@@ -31,12 +36,40 @@ function validate() {
     var szuro2 = /^[0-9a-z\.-]+@([0-9a-z-]+\.)+[a-z]{2,4}$/;
 //    var szuro2 = /^[n-p]/;
     if (!szuro2.test(emailInput)) {
-        hiba += "Érvényes e-mail címet adjon meg!";
+        hiba +="<br>"+ "Érvényes e-mail címet adjon meg!";
         ID("email").style.border = "1px solid red";
     } else {
-        urlapAdatok += "E-mail:" + emailInput + "<br>";
+        urlapAdatok +="<br>"+ "E-mail:" + emailInput + "<br>";
         ID("email").style.border = "none";
     }
+    if (emailInput !== email_ujraInput) {
+        hiba += "<br>"+ "Nem egyezik mega a két e-mail cím!";
+        ID("email_ujra").style.border = "1px solid red";
+    } else {
+        urlapAdatok +="<br>"+  "E-mail cím 2-szor is megadva:" + email_ujraInput + "<br>";
+        ID("email_ujra").style.border = "none";
+    }
+    var szuro3 =/^[+][3][6][-\s\./0-9]*$/;
+    if (!szuro3.test(telefonInput)){
+        hiba +="<br>"+ "Érvényes telefonszámot adjon meg!";
+        ID("telefon").style.border = "1px solid red";
+    }
+    else{
+        urlapAdatok +="<br>"+  "Telefonszám megadva:" + telefonInput + "<br>";
+        ID("telefon").style.border = "none";        
+    }
+   
+      var szuro4=/^[([0-9a-z-]+\.+[a-z]{2,4}$/;
+      if (!szuro4.test(webInput)){
+        hiba +="<br>"+ "Érvényes webcímet adjon meg!";
+        ID("web").style.border = "1px solid red";
+    }
+    else{
+        urlapAdatok +="<br>"+  "Weboldal megadva:" + webInput + "<br>";
+        ID("web").style.border = "none";        
+    }
+    
+
 
 
     $("aside section:nth-child(1) p")[0].innerHTML = hiba;
